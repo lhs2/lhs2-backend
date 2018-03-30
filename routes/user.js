90a88ffa2fc7f669e38b6150a6b05c55da41dfdb
Luiz Henrique <lhs2@cin.ufpe.br>
2018-03-29 23:25:17 -0300

    Implementação do /authenticate para permissão de login com base no banco de dados.

--- routes/user.js
+++ routes/user.js
@@ -18,6 +18,21 @@ MongoClient.connect(url, function(err,database){
     });
   });
 
+  router.post('/authenticate', function(req,res, next) {
+    db.collection("user").findOne({login: req.body.login, password: req.body.password}, {username: 1, _id: 0}, function(err, result) {
+      if(err) {
+        res.status(400).send(err);
+      }
+      else if(result){
+        console.log(result);
+        res.status(200).json({message: "SUCESS", data:{username: result.username }});
+      }
+      else {
+        console.log(result, "prob null");
+        res.status(400).json({message: "INVALID_CREDENTIALS"});
+      }
+    })
+  });
 })
 /* GET users listing. */
 
