c0c29205d1400f4c4553c3335a207670f4ceb7bc
Luiz Henrique <lhs2@cin.ufpe.br>
2018-03-29 22:58:48 -0300

    Adição do mongodb e criação de uma requisição get para testes de funcionamento da conectividade com o Mongo.

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,26 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express'),
+    router = express.Router(),
+    MongoClient = require('mongodb').MongoClient,
+    assert = require('assert');
 
+var url = "mongodb://localhost:27017/lhs2";
+
+MongoClient.connect(url, function(err,database){
+  assert.equal(err, null);
+  const db = database.db('lhs2')
+  console.log("MongoDB sucessfully connected to database lhs2");
+
+  // return all users from db
+  router.get('/showAllUsers', function(req, res, next) {
+    db.collection("user").find({}, {username: 1, _id: 0}).toArray(function (err,docs) {
+      res.status(200).send({message: "Sucess", body: {data: docs}});
+      
+    });
+  });
+
+})
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+
 
 module.exports = router;
+
